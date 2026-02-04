67effc7da9cf6661482f22040828f8bb6c7c2818
Nicolas <nicolasrosselliuchimura@gmail.com>
2026-02-04 01:26:15 +0000

    feat: random numbers gen

--- main.c
+++ main.c
@@ -11,3 +11,19 @@ typedef struct {
     int count;
 } SimulationResults;
 
+// When talking about Monte Carlo, there's the random numbers gen:
+
+double random_uniform(void) {
+    return (double)rand() / (double)RAND_MAX; // Random numbers between 0 and 1;
+}
+//Box Muller for normal distro
+double random_normal(double mean, double std_dev) {
+    static int has_spare = 0;
+    static double spare;
+
+    if (has_spare) {
+        has_spare = 0;
+        return mean + std_dev * spare;
+    }
+
+}
